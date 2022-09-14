a501b877aec66b5ac24777537d8e1903a02d155c
AntonC40 <50184101+AntonC40@users.noreply.github.com>
2022-09-14 01:59:34 -0400

    Add files via upload

---
+++ ProdcutsAPI/src/main/java/com/backend/ProdcutsAPI/ProdcutsApiApplication.java
@@ -0,0 +1,13 @@
+package com.backend.ProdcutsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdcutsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdcutsApiApplication.class, args);
+	}
+
+}
---
+++ ProdcutsAPI/src/test/java/com/backend/ProdcutsAPI/ProdcutsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ProdcutsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdcutsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
